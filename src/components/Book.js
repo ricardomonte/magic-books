b0e77464d624cbf293f91f5e47c1a7e1eb31be26
ricardo <ramn21@gmail.com>
2021-04-16 19:28:24 +0200

    Add button to remove book

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,19 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function Book({ book }) {
+function Book({ book, onRemoveBook }) {
   const { id, title, category } = book;
   return (
     <tr key={id}>
       <td>{id}</td>
       <td>{title}</td>
       <td>{category}</td>
+      <td>
+        <button type="button" value={id} onClick={onRemoveBook}>
+          Remove Book
+        </button>
+      </td>
     </tr>
   );
 }
 
 Book.propTypes = {
   book: PropTypes.instanceOf(Object).isRequired,
+  onRemoveBook: PropTypes.func.isRequired,
 };
 
 export default Book;
