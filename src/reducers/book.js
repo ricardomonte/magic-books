c8a58d16f0a986c731887082f214e1bd3a539be4
ricardo <ramn21@gmail.com>
2021-04-16 19:41:33 +0200

    Add faker to generate id

--- src/reducers/book.js
+++ src/reducers/book.js
@@ -1,9 +1,17 @@
+import faker from 'faker';
 import * as types from '../actions/actionTypes';
 
 function bookReducer(state = [], action) {
   switch (action.type) {
     case types.CREATE_BOOK:
-      return [...state, action.payload];
+      return [
+        ...state,
+        {
+          id: faker.finance.routingNumber(),
+          title: action.payload.title,
+          category: action.payload.category,
+        },
+      ];
     case types.REMOVE_BOOK:
       return [...state].filter((element) => element.id !== action.payload.id);
     default:
