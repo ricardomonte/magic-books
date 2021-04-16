ed6e7841828cb1b5077e94b465c7e321c3cdbd36
ricardo <ramn21@gmail.com>
2021-04-16 03:21:42 +0200

    Add initial state, add provider

--- src/index.js
+++ src/index.js
@@ -1,11 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { Provider } from 'react-redux';
 import App from './components/App';
+import configureStore from './configureStore';
+
+const State = {
+  book: [
+    {
+      id: Math.floor(Math.random() * 10).toString(10),
+      title: 'La inevitable levedad del ser',
+      category: 'Philosophy',
+    },
+    {
+      id: Math.floor(Math.random() * 10).toString(10),
+      title: 'IT',
+      category: 'Horror',
+    },
+    {
+      id: Math.floor(Math.random() * 10).toString(10),
+      title: 'React for Dummies',
+      category: 'Learning',
+    },
+    {
+      id: Math.floor(Math.random() * 10).toString(10),
+      title: 'Redux for Dummies',
+      category: 'Learning',
+    },
+  ],
+};
+
+const store = configureStore(State);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
   document.getElementById('root'),
 );
