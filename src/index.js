57d64f6a2f476a64e04020f60a7c78016883c99f
ricardo <ramn21@gmail.com>
2021-04-23 01:53:39 +0200

    Delete const state in index

--- src/index.js
+++ src/index.js
@@ -2,37 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 import { Provider } from 'react-redux';
-import { v4 as uuidv4 } from 'uuid';
 import App from './components/App';
 import configureStore from './configureStore';
 import NavBar from './common/NavBar';
 
-const State = {
-  book: [
-    {
-      id: uuidv4(),
-      title: 'La inevitable levedad del ser',
-      category: 'Philosophy',
-    },
-    {
-      id: uuidv4(),
-      title: 'IT',
-      category: 'Horror',
-    },
-    {
-      id: uuidv4(),
-      title: 'React for Dummies',
-      category: 'Learning',
-    },
-    {
-      id: uuidv4(),
-      title: 'Redux for Dummies',
-      category: 'Learning',
-    },
-  ],
-};
-
-const store = configureStore(State);
+const store = configureStore();
 
 ReactDOM.render(
   <Provider store={store}>
