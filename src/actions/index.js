a48cadfeb1a579bb233706de5364cde8e3d7d36e
ricardo <ramn21@gmail.com>
2021-04-23 01:58:50 +0200

    Add thunks for load, create and remove

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,11 +1,5 @@
 import * as types from './actionTypes';
-
-export function createBook(book) {
-  return {
-    type: types.CREATE_BOOK,
-    payload: book,
-  };
-}
+import * as bookApi from '../api/ApiBooks';
 
 export function removeBook(book) {
   return {
@@ -18,5 +12,60 @@ export function changeFilter(filter) {
   return {
     type: types.CHANGE_FILTER,
     payload: filter,
+  };
+}
+
+export function loadBooksSuccess(books) {
+  return {
+    type: types.LOAD_BOOK_SUCCESS,
+    payload: books,
+  };
+}
+
+export function createBookSuccess(book) {
+  return {
+    type: types.CREATE_BOOK_SUCCESS,
+    payload: book,
+  };
+}
+
+export function deleteBookSuccess(book) {
+  return {
+    type: types.DELETE_BOOK_SUCCESS,
+    payload: book,
+  };
+}
+
+export function loadBooks() {
+  return function (dispatch) {
+    return bookApi
+      .GetAllBooks()
+      .then((books) => {
+        dispatch(loadBooksSuccess(books));
+      })
+      .catch((error) => {
+        throw error;
+      });
+  };
+}
+
+export function saveBook(book) {
+  return function (dispatch) {
+    return bookApi
+      .createBook(book)
+      .then((savedBook) => {
+        dispatch(createBookSuccess(savedBook));
+      })
+      .catch((error) => {
+        throw error;
+      });
+  };
+}
+
+export function deleteBook(book) {
+  return function (dispatch) {
+    return bookApi.removeBook(book).then(() => {
+      dispatch(deleteBookSuccess(book));
+    });
   };
 }
